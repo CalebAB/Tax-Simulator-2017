a72dea0fc49b1731c7b67c006dc8fdd8f1608ad5
Caleb B <calebbirdxyz@gmail.com>
2016-05-26 14:18:09 -0400

    More stuff

--- Tax Simulator.py
+++ Tax Simulator.py
@@ -1,10 +1,22 @@
 '''
 Tax Simulator 2017, funded by Futur Media
 '''
-Gross = 0;
-Net = 0;
-while True:
-    input('How much money did you make the last year')
-    Gross = float(input)
-    
+Gross = 0.00
+Owed = 0.00
+Net = 0.00
+
+In = input('How much money did you make the last year')
+Gross = float(In)
+Owed = Gross * 0.30
+
+In = input('You will owe' + str(Owed) + 'Do you want to commit massive tax fraud? (yes or no)')
+
+if In in ['yes','Yes','YES']:
+    Owed = 0
+    Net = Gross - Owed
+
+if In in ['no','No','NO']:
+    Net = Gross - Owed
+
+print('Your net this year is' + str(Net))
     
